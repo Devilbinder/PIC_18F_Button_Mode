411a5a4c1326549a7be8014f29343d0656ddb46f
Devilbinder <Devilbinder@users.noreply.github.com>
2018-09-18 22:02:49 +0200

    first

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# PIC_18F_Button_Mode
+# PIC Running LEDs
+This code Demonstrates how to do a mode swap LED with the XC8 compiler with MPLAB X and a PIC18F4520.
+
+<a href="https://youtu.be/OBNpr4BSHA8">PIC Programming Tutorial #3 - Blinky and Running LEDs</a>
+
+<a href="https://amzn.to/2oTHRqm">PIC18f4520</a>
+
+<a href="https://amzn.to/2p2PsmV">PIC18f4520  (Alternative)</a>
+
+<a href="https://amzn.to/2x5Fq8a">LED Kit</a>
+
+<a href="https://amzn.to/2COwEBA">Resistor Kit</a>
+
+<a href="https://amzn.to/2BzKsiE">Pickit3 Programmmer (compatible)</a>
+
+<a href="https://www.microchip.com/Developmenttools/ProductDetails/PartNo/PG164130">Pickit3 Programmmer (original)</a>
+
---
+++ button.X/main.c
@@ -0,0 +1,42 @@
+#include <xc.h>
+#include <p18f4520.h>
+#include "conbits.h"
+
+char run =1;
+char tracking =0;
+
+void main(void) {
+    TRISB=0; //0b00000000
+    TRISDbits.RD7=1; //0b10000000
+    TRISDbits.RD6=1; //0b11000000
+    
+    LATB=0; //0b00000000
+   
+    while(1){
+        
+        if(PORTDbits.RD7==1){ //0b10000000
+            __delay_ms(250);
+            if(tracking == 0){
+                tracking = 1;
+            }else if(tracking == 1){
+                tracking = 0;
+            }
+            while(PORTDbits.RD7==1);
+        }
+        
+        if(run >= 0x80){ //0b10000000
+                run = 1;
+            }else{
+                run *= 2; //0b00001000
+        }
+        if(tracking == 0){
+            LATB=run; 
+        }
+        if(tracking == 1){
+            LATB=~run;     
+        }
+        
+        __delay_ms(250);
+    }
+    
+}
